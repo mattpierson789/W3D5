469ca04c9053919897d6f9079fe2b75cc2bade25
Tyvan Cheng <tyvancheng@gmail.com>
2023-03-24 15:18:03 -0400

    added parent and init v2

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -7,21 +7,18 @@ class PolyTreeNode
     end
 
     def parent=(node)
-        # p node
-        # p self
+  
         if self.parent != node && self.parent != nil
             if self.parent.children.include?(self)
-                self.parent.children.drop(1)
+                self.parent.children.delete(self)
             end
-        # node.children.each do |child|
-        #     child.parent = node
         end
         @parent = node
-        # if !node.children.include?(self)
+        if (node != nil) && (!node.children.include?(self))
             node.children.push(self)
-        # end
-        # self.parent.children << self
-        # node.parent.children
+        end
+      
     end
+
     
 end
